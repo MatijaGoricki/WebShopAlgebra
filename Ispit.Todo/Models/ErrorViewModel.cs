54ef55ff3ee199c7e8485f73289c81b0426c8808
Matija <matija.goricki1@gmail.com>
2023-11-03 18:05:08 +0100

    Delete Ispit.Todo directory

--- Ispit.Todo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ispit.Todo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
