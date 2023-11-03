778925c1ebfbc6921021fa785c58b54c0948b383
Matija Goricki <matija.goricki1@gmail.com>
2023-11-03 17:59:55 +0100

    Add project files.

---
+++ Movies/Controllers/AdminOrderController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Movies.Data;
+using Movies.Models;
+
+namespace Movies.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminOrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminOrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private List<OrderItem> ItemsForOrder(int orderid)
+        {
+            return (
+                from order_item in _context.OrderItem
+                where order_item.OrderId == orderid
+                select new OrderItem
+                {
+                    Id = order_item.Id,
+                    OrderId = order_item.OrderId,
+                    ProductId = order_item.ProductId,
+                    Quantity = order_item.Quantity,
+                    Price = order_item.Price,
+                    ProductTitle = (from product in _context.Product
+                                    where product.Id == order_item.ProductId
+                                    select product.Title).FirstOrDefault()
+                }
+                ).ToList();
+        }
+
+        // GET: AdminOrder
+        public async Task<IActionResult> Index()
+        {
+              return _context.Order != null ? 
+                          View(await _context.Order.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Order'  is null.");
+        }
+
+        // GET: AdminOrder/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            order.OrderItems=ItemsForOrder(order.Id);
+
+            return View(order);
+        }
+
+        // GET: AdminOrder/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            order.OrderItems = ItemsForOrder((int)id);
+            return View(order);
+        }
+
+        // POST: AdminOrder/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DateCreated,Total,BillingFirstName,BillingLastName,BillingEmail,BillingPhone,BillingAddress,BillingCity,BillingPostalCode,BillingCountry,ShippingFirstName,ShippingLastName,ShippingEmail,ShippingPhone,ShippingAddress,ShippingCity,ShippingPostalCode,ShippingCountry,Message,UserId")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+            ModelState.Remove("OrderItems");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            order.OrderItems = ItemsForOrder((int)id);
+            return View(order);
+        }
+
+        // GET: AdminOrder/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            order.OrderItems = ItemsForOrder((int)id);
+            return View(order);
+        }
+
+        // POST: AdminOrder/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Order == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Order'  is null.");
+            }
+            var order = await _context.Order.FindAsync(id);
+            if (order != null)
+            {
+                foreach (var order_item in _context.OrderItem.Where(oi => oi.OrderId == order.Id))
+                {
+                    _context.Product.Find(order_item.ProductId).Quantity += order_item.Quantity;
+                    _context.OrderItem.Remove(order_item);
+                }
+                _context.Order.Remove(order);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+          return (_context.Order?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Movies/Controllers/HomeController.cs
@@ -0,0 +1,242 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Data;
+using Movies.Extensions;
+using Movies.Models;
+using System.Diagnostics;
+
+namespace Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string SessionKeyName = "_cart";
+
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ApplicationDbContext _context;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger,
+                                    ApplicationDbContext context, 
+                                    UserManager<ApplicationUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index(string? message)
+        {
+            return View("Index",message);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Product(int? id, int? categoryId)
+        {
+            List<Product> products = _context.Product.Where(x => x.Active == true).ToList();
+
+            if (id != null)
+            {
+                var product = products.Where(p => p.Id == id).FirstOrDefault();
+                product.ProductImages = _context.ProductImage.Where(pi => pi.ProductId == product.Id).ToList();
+                product.ProductCategories = _context.ProductCategory.Where(pc => pc.ProductId == product.Id).ToList();
+                return View("ProductDetails", product);
+            }
+
+            foreach (var product in products)
+            {
+                product.ProductImages = _context.ProductImage.Where(pi => pi.ProductId == product.Id).ToList();
+                product.ProductCategories = _context.ProductCategory.Where(pc => pc.ProductId == product.Id).ToList();
+            }
+
+            if (categoryId != null)
+            {
+                products = products.Where(p => p.ProductCategories.Any(p => p.CategoryId == categoryId)).ToList();
+            }
+
+            ViewBag.Categories = _context.Category.ToList();
+            return View(products);
+        }
+
+        [Authorize]
+        public IActionResult Order(List<string> errors)
+        {
+            List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>(SessionKeyName);
+            if (cart == null)
+            {
+                return RedirectToAction("Index");
+            }
+            if (cart.Count == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            for (int i = 0; i < cart.Count; i++)
+            {
+                var product = _context.Product.Find(cart[i].Product.Id);
+                if (product == null)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product not found and was removed from cart!");
+                    continue;
+                }
+                if (product.Quantity < cart[i].Quantity)
+                {
+                    cart[i].Quantity = product.Quantity;
+                    errors.Add("Product quantity was reduced to available quantity!");
+                }
+                if(product.Quantity == 0)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product "+ product.Title +" is out of stock and was removed from cart!");
+                    continue;
+                }
+                if (!product.Active)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product "+ product.Title +" is not active and was removed from cart!");
+                    continue;
+                }
+            }
+            HttpContext.Session.SetObjectAsJson(SessionKeyName, cart);
+
+            foreach (CartItem item in cart)
+            {
+                item.Product.ProductImages = _context.ProductImage.Where(pi => pi.ProductId == item.Product.Id).ToList();
+                item.Product.ProductCategories = _context.ProductCategory.Where(pc => pc.ProductId == item.Product.Id).ToList();
+            }
+
+            ViewBag.Errors = errors;
+
+            return View(cart);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult CreateOrder(Order order,string shippingsameaspersonal)
+        {
+            List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>(SessionKeyName);
+            if(cart == null)
+            {
+                return RedirectToAction("Index",new {Message="Cart is empty, so no order can be completed"});
+            }
+            if(cart.Count == 0)
+            {
+                return RedirectToAction("Index", new { Message = "Cart is empty, so no order can be completed" });
+            }
+
+            var errors = new List<string>();
+
+            for (int i = 0; i < cart.Count; i++)
+            {
+                var product = _context.Product.Find(cart[i].Product.Id);
+                if (product == null)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product not found and was removed from cart!");
+                    continue;
+                }
+                if (product.Quantity < cart[i].Quantity)
+                {
+                    cart[i].Quantity = product.Quantity;
+                    errors.Add("Product quantity was reduced to available quantity!");
+                }
+                if (product.Quantity == 0)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product " + product.Title + " is out of stock and was removed from cart!");
+                    continue;
+                }
+                if (!product.Active)
+                {
+                    cart.RemoveAt(i);
+                    i--;
+                    errors.Add("Product is not active and was removed from cart!");
+                    continue;
+                }
+            }
+            HttpContext.Session.SetObjectAsJson(SessionKeyName, cart);
+
+            if(errors.Count > 0)
+            {
+                return RedirectToAction("Order", new { errors = errors });
+            }
+
+            if(shippingsameaspersonal=="on")
+            {
+                order.ShippingFirstName = order.BillingFirstName;
+                order.ShippingLastName = order.BillingLastName;
+                order.ShippingEmail = order.BillingEmail;
+                order.ShippingPhone = order.BillingPhone;
+                order.ShippingAddress = order.BillingAddress;
+                order.ShippingCity = order.BillingCity;
+                order.ShippingPostalCode = order.BillingPostalCode;
+                order.ShippingCountry = order.BillingCountry;
+            }
+            order.DateCreated = DateTime.Now;
+            order.Total=cart.Sum(x=>x.Product.Price*x.Quantity);
+            order.UserId = _userManager.GetUserId(User);
+            
+            ModelState.Remove("Id");
+            ModelState.Remove("OrderItems");
+            ModelState.Remove("shippingsameaspersonal");
+            if (ModelState.IsValid)
+            {
+                _context.Order.Add(order);
+                _context.SaveChanges();
+
+                int order_id = order.Id;
+
+                foreach (var item in cart)
+                {
+                    OrderItem order_item=new OrderItem
+                    {
+                        OrderId=order_id,
+                        ProductId=item.Product.Id,
+                        Quantity=item.Quantity,
+                        Price=item.Product.Price
+                    };
+                    
+                    _context.OrderItem.Add(order_item);
+                    _context.Product.Find(item.Product.Id).Quantity -= item.Quantity;
+                    _context.SaveChanges();
+                }
+
+                HttpContext.Session.Remove(SessionKeyName);
+                //HttpContext.Session.SetObjectAsJson(SessionKeyName,"");
+
+                return RedirectToAction("Index", new {message="Thank you for your order :)"});
+            }
+            else
+            {
+                errors.Add("Order is not valid!");
+                foreach(var modelState in ModelState.Values)
+                {
+                    foreach (var modelError in modelState.Errors)
+                    {
+                        errors.Add(modelError.ErrorMessage);
+                    }
+                }
+            }
+
+            return RedirectToAction("Order", new { errors = errors });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
